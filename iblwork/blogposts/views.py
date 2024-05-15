5869ed1b7053f424925f59cbf44be7d82dd5f681
RUSUF <RUSUF@MacBook-Pro.local>
2024-05-16 02:04:29 +0300

    first

---
+++ iblwork/blogposts/views.py
@@ -0,0 +1,14 @@
+from multiprocessing import context
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import BlogPost
+
+
+
+# Create your views here.
+def index(request):
+    context={
+        "posted": BlogPost.objects.all()
+    }
+
+    return render(request, "posted/index.html", context)
